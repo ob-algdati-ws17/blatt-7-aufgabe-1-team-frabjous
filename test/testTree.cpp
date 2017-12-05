473a958b3c764774802399b55f24b05a61599906
brumma-man <simon.thum@me.com>
2017-12-05 12:31:05 +0100

    Test Integration in subfolder (as per blatt5)

---
+++ test/testTree.cpp
@@ -0,0 +1,14 @@
+//
+// Created by hm-sthum on 05.12.2017.
+//
+
+#include "testTree.h"
+#include <memory>
+
+using namespace std;
+
+// empty list
+
+TEST(TreeTest, TreeTest_Trivial) {
+    EXPECT_EQ(1, 1);
+}
---
+++ test/testTree.h
@@ -0,0 +1,16 @@
+//
+// Created by hm-sthum on 05.12.2017.
+//
+
+#ifndef PROJECT_TESTTREE_H
+#define PROJECT_TESTTREE_H
+
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+//#include "tree.h"
+
+class testTree : public ::testing::Test {
+};
+
+#endif //PROJECT_TESTTREE_H
